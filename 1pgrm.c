c087a6d5c13446d32372d5f7fab58be7538c10e3
shriya345 <shriya.rajesh2005@gmail.com>
2025-07-01 08:30:20 +0530

    Update 1pgrm.c

--- 1pgrm.c
+++ 1pgrm.c
@@ -1,42 +1,29 @@
-#include <stdio.h>
+##include <stdio.h>
 
-int partition(int a[],int low,int high)
-{
-    int pivot=a[low],i=low+1,j=high,temp;
-    while(i<=j){
-        while(i<=high&&a[i]<=pivot) i++;
-         while(a[j]>pivot) j--;
-        if(i<j){
-           temp=a[i];
-            a[i]=a[j];
-            a[j]=temp;
-            
+void quicksort(int a[], int low, int high) {
+    int i = low, j = high, pivot = a[(low + high) / 2], temp;
+    while (i <= j) {
+        while (a[i] < pivot) i++;
+        while (a[j] > pivot) j--;
+        if (i <= j) {
+            temp = a[i]; a[i] = a[j]; a[j] = temp;
+            i++; j--;
         }
     }
-    a[low]=a[j];
-    a[j]=pivot;
-    return j;
+    if (low < j) quicksort(a, low, j);
+    if (i < high) quicksort(a, i, high);
 }
 
-    void quicksort(int a[],int low,int high){
-        if(low<high){
-int p=partition(a,low,high);
-        
-            quicksort(a,low,p-1);
-            quicksort(a,p+1,high);
-        }    // Your code here!
-    
-}
-int main(){
-int a[100],n;
-printf("enter the number of ele\n");
-scanf("%d",&n);
-printf("enter the %d elements\n",n);
-for(int i=0;i<n;i++)
-scanf("%d",&a[i]);
-quicksort(a,0,n-1);
-printf("sorted\n");
-for(int i=0; i<n; i++)
-printf("%d ", a[i]);
-printf("\n");
+int main() {
+    int a[100], n, i;
+    printf("Enter number of elements:\n");
+    scanf("%d", &n);
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) scanf("%d", &a[i]);
+
+    quicksort(a, 0, n - 1);
+
+    printf("Sorted elements:\n");
+    for(i = 0; i < n; i++) printf("%d ", a[i]);
+    return 0;
 }
