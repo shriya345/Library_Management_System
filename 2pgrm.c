b758ac8e40b02ceff6230262e056c2aed5fe52f9
shriya345 <shriya.rajesh2005@gmail.com>
2025-07-01 08:30:53 +0530

    Update 2pgrm.c

--- 2pgrm.c
+++ 2pgrm.c
@@ -1,41 +1,50 @@
-#include<stdio.h>
-void merge(int a[],int low,int mid,int high){
-    int b[100],i=low,j=mid+1,k=low;
-    while(i<=mid && j<=high) 
-    b[k++]= (a[i]<a[j]) ? a[i++]:a[j++];
-    while(i<=mid)
-    b[k++]= a[i++];
-    while(j<=high)
-     b[k++]=a[j++];
-    for(i=low;i<=high;i++){
-        a[i]=b[i];
+#include <stdio.h>
+
+void merge(int a[], int low, int mid, int high) {
+    int i = low, j = mid + 1, k = 0, b[100];
+
+    while (i <= mid && j <= high) {
+        if (a[i] < a[j])
+            b[k++] = a[i++];
+        else
+            b[k++] = a[j++];
+    }
+
+    while (i <= mid)
+        b[k++] = a[i++];
+
+    while (j <= high)
+        b[k++] = a[j++];
+
+    j = 0;
+    for (i = low; i <= high; i++) {
+        a[i] = b[j];
+        j++;
     }
 }
-void mergesort(int a[],int low,int high){  
-  
-        int mid=(low+high)/2;
-          if(low < high){
-        mergesort(a,low,mid);
-        mergesort(a,mid+1,high);
-        merge(a,low,mid,high);
-        
+
+void mergesort(int a[], int low, int high) {
+    if (low < high) {
+        int mid = (low + high) / 2;
+        mergesort(a, low, mid);
+        mergesort(a, mid + 1, high);
+        merge(a, low, mid, high);
     }
 }
-
-
-
 
 int main() {
-    int n, a[100];
-    printf("Enter number of elements: ");
+    int a[100], n, i;
+    printf("Enter number of elements:\n");
     scanf("%d", &n);
     printf("Enter %d elements:\n", n);
-    for (int i = 0; i < n; i++)
+    for(i = 0; i < n; i++)
         scanf("%d", &a[i]);
 
     mergesort(a, 0, n - 1);
 
     printf("Sorted elements:\n");
-    for (int i = 0; i < n; i++)
+    for(i = 0; i < n; i++)
         printf("%d ", a[i]);
+
+    return 0;
 }
