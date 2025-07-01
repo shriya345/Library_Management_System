b17b1184ca51c1679229db113b064f7674be1c8a
shriya345 <shriya.rajesh2005@gmail.com>
2025-07-01 13:18:04 +0530

    Update 10pgrm.c

--- 10pgrm.c
+++ 10pgrm.c
@@ -1,40 +1,56 @@
-#include <stdio.h>
+##include <stdio.h>
 
-void dijkstra(int n, int a[10][10], int source) {
-    int d[100], s[10], i, j, u, v, minval, sum = 0;
-    for (i = 1; i <= n; i++) {
-        s[i] = 0;
-        d[i] = a[source][i];
-    }
-    s[source] = 1;
-    for (i = 1; i <= n; i++) {
-        minval = 999;
-        for (j = 1; j <= n; j++) {
-            if (s[j] == 0 && d[j] < minval) {
-                minval = d[j];
+int dijkstra(int n, int a[10][10], int src) {
+    int visited[10] = {0}, dist[10];
+    int i, j, u, v, min;
+
+    for(i = 1; i <= n; i++)
+        dist[i] = a[src][i];
+
+    visited[src] = 1;
+
+    for(i = 1; i < n; i++) {
+        min = 999;
+        for(j = 1; j <= n; j++) {
+            if(!visited[j] && dist[j] < min) {
+                min = dist[j];
                 u = j;
             }
         }
-        s[u] = 1;
-        sum += d[u];
-        for (v = 1; v <= n; v++) {
-            if (s[v] == 0 && d[v] > d[u] + a[u][v])
-                d[v] = d[u] + a[u][v];
+
+        visited[u] = 1;
+
+        for(v = 1; v <= n; v++) {
+            if(!visited[v] && dist[v] > dist[u] + a[u][v])
+                dist[v] = dist[u] + a[u][v];
         }
     }
-    for (i = 1; i <= n; i++)
-        printf("The shortest distance from %d to %d is %d\n", source, i, d[i]);
+
+    for(i = 1; i <= n; i++)
+        printf("The shortest distance from %d to %d is %d\n", src, i, dist[i]);
+
+    return 0;
 }
 
 int main() {
-    int n, a[10][10], i, j, source;
-    printf("Enter the number of nodes:\n");
+    int n, a[10][10], i, j, src;
+
+    printf("Enter number of nodes:\n");
     scanf("%d", &n);
-    printf("Enter the adjacency matrix:\n");
-    for (i = 1; i <= n; i++)
-        for (j = 1; j <= n; j++)
+
+    printf("Enter adjacency matrix:\n");
+    for(i = 1; i <= n; i++) {
+        for(j = 1; j <= n; j++) {
             scanf("%d", &a[i][j]);
+            if(i != j && a[i][j] == 0)
+                a[i][j] = 999;
+        }
+    }
+
     printf("Enter the source node:\n");
-    scanf("%d", &source);
-    dijkstra(n, a, source);
+    scanf("%d", &src);
+
+    dijkstra(n, a, src);
+
+    return 0;
 }
